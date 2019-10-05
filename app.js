490e0065073e800530ef33d73658c505ae5c9205
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-10-05 16:11:02 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+//const layout = require('./views/layout');
+
+const app = express();
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, './public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('abc');
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const app = require('./app');
+const server = http.createServer(app);
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`);
+});
