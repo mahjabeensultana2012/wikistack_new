5d040b4e1733abc9264f7a3cf12234a9505dae1d
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-10-08 12:35:36 -0500

    route new page

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+const { addPage } = require('../views');
 router.get('/', (req, res, next) => {
   res.send('got to /wiki');
 });
+
+router.post('/', (req, res, next) => {
+  res.send('got to /wiki');
+});
+
+// router.get('/add', (req, res, next) => {
+//   res.send(addPage());
+// });
+
 module.exports = router;
