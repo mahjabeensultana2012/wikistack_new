5bc1f462285f6dd1248bb0886df38095101a8298
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-10-08 13:34:12 -0500

    add form

--- views/addPage.js
+++ views/addPage.js
@@ -1,29 +1,51 @@
-const html = require("html-template-tag");
-const layout = require("./layout");
+const html = require('html-template-tag');
+const layout = require('./layout');
 
-module.exports = () => layout(html`
-  <h3>Add a Page</h3>
-  <hr>
-  <form method="POST" action="/wiki/">
-    
-    <div>PLACEHOLDER FOR AUTHOR NAME FIELD</div>
-    
-    <div>PLACEHOLDER FOR AUTHOR EMAIL FIELD</div>
-    
-    <div class="form-group">
-      <label for="title" class="col-sm-2 control-label">Page Title</label>
-      <div class="col-sm-10">
-        <input id="title" name="title" type="text" class="form-control"/>
+module.exports = () =>
+  layout(html`
+    <h3>Add a Page</h3>
+    <hr />
+    <form method="POST" action="/wiki/">
+      <div class="form-group">
+        <label for="name" class="col-sm-2 control-label">Author Name</label>
+        <div class="col-sm-10">
+          <input id="name" name="name" type="text" class="form-control" />
+        </div>
       </div>
-    </div>
 
-    <div>PLACEHOLDER FOR PAGE CONTENT TEXTAREA FIELD</div>
-    
-    <div>PLACEHOLDER FOR PAGE STATUS INPUT FIELD</div>
+      <div class="form-group">
+        <label for="email" class="col-sm-2 control-label">Email</label>
+        <div class="col-sm-10">
+          <input id="email" name="email" type="text" class="form-control" />
+        </div>
+      </div>
 
-    <div class="col-sm-offset-2 col-sm-10">
-      <button type="submit" class="btn btn-primary">submit</button>
-    </div>
-  
-  </form>
-`);
+      <div class="form-group">
+        <label for="title" class="col-sm-2 control-label">Title</label>
+        <div class="col-sm-10">
+          <input id="title" name="title" type="text" class="form-control" />
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="content" class="col-sm-2 control-label">Content</label>
+        <div class="col-sm-10">
+          <textarea name="content"></textarea>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="status" class="col-sm-2 control-label">Status</label>
+        <div class="col-sm-10">
+          <select name="status">
+            <option>open</option>
+            <option>closed</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-primary">submit</button>
+      </div>
+    </form>
+  `);
