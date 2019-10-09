432708be3bbc141f38b923cd013c359d6c07aed4
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-10-09 12:15:49 -0500

    data saved in db

--- models/index.js
+++ models/index.js
@@ -6,6 +6,10 @@ db.authenticate().then(() => {
   console.log('connected to the database');
 });
 
+function generateSlug(title) {
+  return title.replace(/\s+/g, '_').replace(/\W/g, '');
+}
+
 const Page = db.define('page', {
   title: {
     type: Sequelize.STRING,
@@ -22,6 +26,12 @@ const Page = db.define('page', {
   status: {
     type: Sequelize.ENUM('open', 'closed'),
   },
+});
+
+Page.beforeValidate(page => {
+  if (!page.slug) {
+    page.slug = generateSlug(page.title);
+  }
 });
 
 const User = db.define('user', {
