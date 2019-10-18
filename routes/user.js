b7e6af69750ee25d647425e0b44cfb2b94510be4
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-10-18 12:51:16 -0500

    get users

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+const { Page, User } = require('../models');
+const {
+  addPage,
+  editPage,
+  main,
+  userList,
+  userPages,
+  wikiPage,
+  layout,
+} = require('../views');
+
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    res.send(userList(users));
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
